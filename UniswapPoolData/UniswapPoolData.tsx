3a38d99feed83e0d94b80d5938cdd94ffa3310e2
Stacey Burns <5744432+staceb@users.noreply.github.com>
2023-07-14 15:41:52 +0100

    added liquidity data to UniswapPoolData hook

--- UniswapPoolData/UniswapPoolData.tsx
+++ UniswapPoolData/UniswapPoolData.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { PoolData, PoolDataRaw, PoolDayData } from "./UniswapPoolData.type"
+import { PoolData, PoolDataRaw, PoolDayData, PoolTickData, PoolLiquidityData } from "./UniswapPoolData.type"
 import { fetchPoolDataById } from './queries/pools'
 import { stdSample } from '../helpers/std'
 import { sumArray } from '../helpers/numbers'
@@ -34,8 +34,54 @@ const processPoolData = ( poolData: PoolDataRaw ): PoolData =>{
     currentPrice: 0,
     std: stats.std,
     mean: stats.mean,
-    normStd: stats.normStd
+    normStd: stats.normStd,
+    liquidity: genLiquidityData(poolData.ticks, poolData.feeTier)
   };
+}
+
+const genLiquidityData = ( data:PoolTickData[], feeTier:number ): PoolLiquidityData[] => {
+
+  const calcTvl = ( liquidity:number, feeTier:number, price:number, decimals:number[] ): number[] => {
+
+    const multiplier = 1 + (feeTier / 500000);
+    const T = liquidity * Math.sqrt(price);
+    const H = liquidity / Math.sqrt(price * multiplier);
+
+    const amount0 = ((Math.pow(liquidity, 2) / T) - H)  / Math.pow(10, decimals[1]);
+    const amount1 = ((Math.pow(liquidity, 2 ) / H) - T) / Math.pow(10, decimals[0]);
+
+    return [amount0, amount1];
+
+  }
+
+  let cumsum:number = 0;
+  const multiplier:number = 1 + (feeTier / 500000);
+  const len:number = data.length;
+
+  return data.map((d, i) => {
+
+    cumsum += d.liquidityNet;
+    const T:number = cumsum * Math.sqrt(d.price0);
+    const H:number = cumsum / Math.sqrt(d.price0 * multiplier);
+    const nextRecord:number = Math.min((len - 1), (i + 1));
+    const width:number = Math.abs(data[nextRecord].tickIdx)  - d.tickIdx;
+
+    return {
+      ...d, 
+      decimal: d.pool.token0.decimals - d.pool.token1.decimals,
+      liquidity: cumsum,
+      width: width,
+      tvlAmount0: ((Math.pow(cumsum, 2) / T) - H) / Math.pow(10, d.pool.token0.decimals),
+      tvlAmount1: ((Math.pow(cumsum, 2 ) / H) - T) / Math.pow(10, d.pool.token1.decimals),
+      price0: d.price0,
+      price1: d.price1,
+      price0N: Math.pow(1.0001, d.tickIdx) / Math.pow(10, (d.pool.token1.decimals - d.pool.token0.decimals)),
+      price1N: Math.pow(1.0001, d.tickIdx * -1) / Math.pow(10, (d.pool.token0.decimals - d.pool.token1.decimals)),
+      tickIdx0: d.tickIdx,
+      tickIdx1: (d.tickIdx * -1) - width
+    }
+  });
+  
 }
 
 const emptyDataSetError = (id: string, protocol:number) => {
@@ -62,8 +108,7 @@ export default function useUniswapPoolData( ) {
         setError(emptyDataSetError(id, protocol));
       }
       if ( d && d.hasOwnProperty("id")) {
-        setPoolDataRaw(d.id[0]);
-      
+        setPoolDataRaw(d.id[0]);   
       }
       setLoading(false);
     });
--- UniswapPoolData/UniswapPoolData.type.ts
+++ UniswapPoolData/UniswapPoolData.type.ts
@@ -1,88 +1,103 @@
 interface TokenData {
-  "id": string,
-  "symbol": string,
-  "name": string,
-  "decimals": number,
+  id: string,
+  symbol: string,
+  name: string,
+  decimals: number,
 }
 
 interface PoolToken {
-  "id": string,
-  "symbol": string,
-  "name": string,
-  "decimals": number,
-  "baseToken"?: boolean, // extract this out
-  "currentPrice"?: number // extract this out
+  id: string,
+  symbol: string,
+  name: string,
+  decimals: number,
+  baseToken?: boolean, // extract this out
+  currentPrice?: number // extract this out
 }
 
 export interface PoolDayData {
-    "date": number,
-    "volumeUSD": number,
-    "tvlUSD": number,
-    "feesUSD": number,
-    "liquidity": number,
-    "high": number,
-    "low": number,
-    "volumeToken0": number,
-    "volumeToken1": number,
-    "close": number,
-    "open": number,
-    "txCount": number
+  date: number,
+  volumeUSD: number,
+  tvlUSD: number,
+  feesUSD: number,
+  liquidity: number,
+  high: number,
+  low: number,
+  volumeToken0: number,
+  volumeToken1: number,
+  close: number,
+  open: number,
+  txCount: number
 }
 
 interface TickToken { // this should be replaced in the code to use the data from PoolToken (if it makes sense performance wise)
-  "decimals": number,
-  "symbol": string
+  decimals: number,
+  symbol: string
 }
 
 interface TickPoolData {
-  "tick": number,
-  "token0Price": number,
-  "token1Price": number,
-  "token0": TickToken,
-  "token1": TickToken
+  tick: number,
+  token0Price: number,
+  token1Price: number,
+  token0: TickToken,
+  token1: TickToken
 }
 
-interface PoolTickData {
-  "tickIdx": string,
-  "pool": TickPoolData,
-  "decimal": number,
-  "liquidityGross": number,
-  "liquidityNet": number,
-  "price0": number,
-  "price1": number,
-  "liquidity": number,
-  "tvlAmount0": number,
-  "tvlAmount1": number,
-  "price0N": number,
-  "price1N": number,
-  "tickIdx0": number,
-  "tickIdx1": number,
-  "width": number,
+export interface PoolTickData {
+  tickIdx: number,
+  pool: TickPoolData,
+  decimal: number,
+  liquidityGross: number,
+  liquidityNet: number,
+  price0: number,
+  price1: number,
+  liquidity: number,
+  tvlAmount0: number,
+  tvlAmount1: number,
+  price0N: number,
+  price1N: number,
+  tickIdx0: number,
+  tickIdx1: number,
+  width: number,
+}
+
+export interface PoolLiquidityData extends PoolTickData {
+  decimal: number,
+  liquidity: number,
+  width: number,
+  tvlAmount0: number,
+  tvlAmount1: number,
+  price0: number,
+  price1: number,
+  price0N: number,
+  price1N: number,
+  tickIdx0: number,
+  tickIdx1: number
 }
 
 export interface PoolDataRaw {
-  "id": string,
-  "feeTier": number,
-  "totalValueLockedUSD": number,
-  "totalValueLockedETH": number,
-  "token0Price": number, 
-  "token1Price": number,
-  "token0": PoolToken,
-  "token1": PoolToken,
-  "poolDayData": PoolDayData[],
-  "ticks": PoolTickData[],
+  id: string,
+  feeTier: number,
+  totalValueLockedUSD: number,
+  totalValueLockedETH: number,
+  token0Price: number, 
+  token1Price: number,
+  token0: PoolToken,
+  token1: PoolToken,
+  poolDayData: PoolDayData[],
+  ticks: PoolTickData[],
 }
 
 export interface PoolData extends PoolDataRaw {
-  "name": string, //
-  "price_base": string,  // find where this is used in the code and change to baseToken.symbol {it holds the name not the price}
-  "price_token": string, // find where this is used in the code and change to baseToken.symbol {it holds the name not the price}
-  "baseToken"?: number, // extract this out
-  "currentPrice"?: number // extract this out
-  // "loading": boolean,
-  "std": number,
-  "mean": number,
-  "normStd": number,
+  name: string, //
+  price_base: string,  // find where this is used in the code and change to baseToken.symbol {it holds the name not the price}
+  price_token: string, // find where this is used in the code and change to baseToken.symbol {it holds the name not the price}
+  baseToken?: number, // extract this out
+  currentPrice?: number // extract this out
+  // loading: boolean,
+  std: number,
+  mean: number,
+  normStd: number,
+  liquidity: PoolLiquidityData[]
 }
 
 export interface Protocol {
